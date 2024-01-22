53520596df54d3add5189276e593987655def071
mvaldes <mvaldes.revilla@gmail.com>
2024-01-22 13:48:32 -0600

    open telescope on load

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -15,7 +15,7 @@ end, {})
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    if vim.fn.argv(0) == "." then
+    if vim.fn.argv(0) == "" then
       require("telescope.builtin").find_files()
     end
   end,
