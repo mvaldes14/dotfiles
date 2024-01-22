a8eed8de853caa2237b09ea439df58bd9f843c09
mvaldes <mvaldes.revilla@gmail.com>
2024-01-22 09:13:32 -0600

    fancy new autocmds

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -20,3 +20,22 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  desc = "Highlight selection on yank",
+  callback = function()
+    vim.highlight.on_yank { timeout = 200, visual = true }
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("vertical_help", { clear = true }),
+  pattern = "help",
+  callback = function()
+    vim.bo.bufhidden = "unload"
+    vim.cmd.wincmd "L"
+    vim.cmd.wincmd "="
+  end,
+})
