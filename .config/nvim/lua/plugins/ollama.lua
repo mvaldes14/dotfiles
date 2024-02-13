2376c85c055799a95bc49ed1cfcd6f1662f07212
mvaldes <mvaldes.revilla@gmail.com>
2024-02-12 22:11:59 -0600

    adding ollama plugin

---
+++ .config/nvim/lua/plugins/ollama.lua
@@ -0,0 +1,26 @@
+return {
+  "nomnivore/ollama.nvim",
+  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+  keys = {
+    {
+      "<leader>oo",
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = "ollama prompt",
+      mode = { "n", "v" },
+    },
+  },
+  opts = {
+    model = "mistral",
+    url = "http://127.0.0.1:11434",
+    prompts = {
+      Fix_Grammar = {
+        prompt = "Fix the grammar and enhance the readability of the following text: \n$buf",
+        action = "replace",
+      },
+      Grammar_Selection= {
+        prompt = "Validate the grammar of the selected text: \n$sel",
+        action = "display",
+      },
+    },
+  },
+}
