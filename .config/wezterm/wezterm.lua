d401ba1ddee40c11b688941191dcf84be3824a87
mvaldes <mvaldes.revilla@gmail.com>
2024-03-04 21:01:34 -0600

    minor updates on wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,22 +7,22 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
-config.font = wezterm.font_with_fallback{"JetBrainsMono NF", "FiraCode Nerd Font","Hack"}
+config.font = wezterm.font_with_fallback({ "JetBrainsMono NF", "FiraCode Nerd Font", "Hack" })
 config.font_size = 20
 config.line_height = 1.0
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.97
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
-config.color_scheme = 'tokyonight_night'
+config.color_scheme = "tokyonight_night"
 config.window_decorations = "RESIZE"
 config.adjust_window_size_when_changing_font_size = false
 config.colors = {
-  background = "#1a1b26",
+	background = "#1a1b26",
 }
 config.window_padding = {
 	top = 5,
