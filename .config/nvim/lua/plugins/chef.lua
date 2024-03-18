84690c7aef0dd2097e104eeb72e15defcb6f1121
a1471283 <a1471283@bestbuy.com>
2024-03-18 08:09:05 -0600

    Redoing plugins and swapping for lua versions

---
+++ .config/nvim/lua/plugins/chef.lua
@@ -0,0 +1,8 @@
+return {
+  "t9md/vim-chef",
+  ft = "chef",
+  keys = {
+    { "<leader>fa", "<cmd>ChefFindAny<cr>", desc = "Chef Find" },
+    { "<leader>fs", "<cmd>ChefFindAnySplit<cr>", desc = "Chef Find (Split)" },
+  },
+}
---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,7 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "sindrets/diffview.nvim", -- optional - Diff integration
+  },
+  config = true,
+}
