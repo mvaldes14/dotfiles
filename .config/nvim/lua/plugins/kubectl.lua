bee29bdefec4932f85d4298953b8d1cc59bbecaf
mvaldes <mvaldes.revilla@gmail.com>
2024-05-27 22:16:09 -0500

    adding new plugins

---
+++ .config/nvim/lua/plugins/kubectl.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "ramilito/kubectl.nvim",
+    keys = {
+      {
+        "<leader>k",
+        function()
+          require("kubectl").open()
+        end,
+        desc = "Kubectl",
+      },
+    },
+    config = function()
+      require("kubectl").setup()
+    end,
+  },
+}
