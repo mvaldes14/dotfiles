f5b44a5baceb3af628f6456c964f55fbf2e6879d
a1471283 <a1471283@bestbuy.com>
2024-07-10 13:44:04 -0500

    updated scripts

--- scripts/tmux-ssh.sh
+++ scripts/tmux-ssh.sh
@@ -6,14 +6,16 @@ if [[ $# -eq 0 ]]; then
 fi
 
 app=$1
+env=$2
 
 tmux neww -n $app
-tmux selectw -t $app
-sleep 3
-
+tmux selectw -t $app -n $app
 # aws ec2 describe-instances --profile "$AWS_DEFAULT_PROFILE" --region "us-east-1" --output text \ 
 # --query "Reservations[*].Instances[*].{Name:Tags[?Key==\`Name\`]|[0].Value,IP:PrivateIpAddress}" \ 
 # --filters "Name=tag:Name,Values=*$app*" | awk {'print $1'} | xargs -I % tmux split ssh %
 
-jws-grep $app | awk '{ print $2 }' | xargs -I % tmux split ssh %
+servers=`jws-grep $app $env | awk '{ print $2 }'` 
 
+for i in $servers; do
+  tmux splitw ssh $i
+done
