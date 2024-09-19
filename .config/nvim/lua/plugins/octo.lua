c7a81910e770aab5d3a1dbe7cf0a57836cc861ef
a1471283 <a1471283@bestbuy.com>
2024-09-19 11:20:52 -0500

    octo super slow

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -5,11 +5,14 @@ return {
     "nvim-telescope/telescope.nvim",
     "nvim-tree/nvim-web-devicons",
   },
+  keys = {
+    { "<leader>gr", "<cmd>Octo<cr>", desc = "Octo" },
+  },
   config = function()
-    require("octo").setup({
-        suppress_missing_scope = {
-          projects_v2 = true,
-        }
-    })
+    require("octo").setup {
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+    }
   end,
 }
