3d3065d842273d47a71d1dd5bfafd940af20665f
a1471283 <a1471283@bestbuy.com>
2024-11-12 10:55:48 -0600

    no avante at work

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -2,14 +2,18 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false,   -- set this if you want to always pull the latest change
+  version = false, -- set this if you want to always pull the latest change
+  cond = function()
+    local validation = require "helper"
+    return validation.check_work()
+  end,
   opts = {
     provider = "ollama",
     use_absolute_path = true,
     vendors = {
       ---@type AvanteProvider
       ollama = {
-        ['local'] = true,
+        ["local"] = true,
         endpoint = "http://localhost:11434/v1",
         model = "codellama:7b-instruct",
         parse_curl_args = function(opts, code_opts)
@@ -18,11 +22,11 @@ return {
             headers = {
               ["Accept"] = "application/json",
               ["Content-Type"] = "application/json",
-              ['x-api-key'] = 'ollama',
+              ["x-api-key"] = "ollama",
             },
             body = {
               model = opts.model,
-              messages = require("avante.providers").copilot.parse_message(code_opts),               -- you can make your own message, but this is very advanced
+              messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
               max_tokens = 2048,
               stream = true,
             },
@@ -34,7 +38,7 @@ return {
       },
     },
     behaviour = {
-      auto_suggestions = false,       -- Experimental stage
+      auto_suggestions = false, -- Experimental stage
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
       auto_apply_diff_after_generation = false,
@@ -43,52 +47,52 @@ return {
     mappings = {
       --- @class AvanteConflictMappings
       diff = {
-        ours = 'co',
-        theirs = 'ct',
-        all_theirs = 'ca',
-        both = 'cb',
-        cursor = 'cc',
-        next = ']x',
-        prev = '[x',
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]x",
+        prev = "[x",
       },
       suggestion = {
-        accept = '<M-l>',
-        next = '<M-]>',
-        prev = '<M-[>',
-        dismiss = '<C-]>',
+        accept = "<M-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
       },
       jump = {
-        next = ']]',
-        prev = '[[',
+        next = "]]",
+        prev = "[[",
       },
       submit = {
-        normal = '<CR>',
-        insert = '<C-s>',
+        normal = "<CR>",
+        insert = "<C-s>",
       },
     },
     hints = { enabled = true },
     windows = {
       ---@type "right" | "left" | "top" | "bottom"
-      position = 'right',         -- the position of the sidebar
-      wrap = true,                -- similar to vim.o.wrap
-      width = 30,                 -- default % based on available width
+      position = "right", -- the position of the sidebar
+      wrap = true, -- similar to vim.o.wrap
+      width = 30, -- default % based on available width
       sidebar_header = {
-        align = 'center',         -- left, center, right for title
+        align = "center", -- left, center, right for title
         rounded = true,
       },
     },
     highlights = {
       ---@type AvanteConflictHighlights
       diff = {
-        current = 'DiffText',
-        incoming = 'DiffAdd',
+        current = "DiffText",
+        incoming = "DiffAdd",
       },
     },
     --- @class AvanteConflictUserConfig
     diff = {
       autojump = true,
       ---@type string | fun(): any
-      list_opener = 'copen',
+      list_opener = "copen",
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -100,7 +104,7 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons",     -- or echasnovski/mini.icons
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -120,7 +124,7 @@ return {
     },
     {
       -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
+      "MeanderingProgrammer/render-markdown.nvim",
       opts = {
         file_types = { "markdown", "Avante" },
       },
