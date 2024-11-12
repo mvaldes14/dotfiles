4ddcdb0c179724dc665ff9ef4ebd7342ed70fc19
mvaldes <mvaldes.revilla@gmail.com>
2024-11-11 23:35:34 -0600

    avante, bye vimux

--- .config/nvim/lua/plugins/vimux.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "preservim/vimux",
-  keys = {
-    { "<leader>to", "<cmd>VimuxPromptCommand<cr>", desc = "[T]mux Prompt" },
-    { "<leader>tc", "<cmd>VimuxCloseRunner<cr>", desc = "[T]mux Close" },
-    { "<leader>tr", "<cmd>VimuxRunLastCommand<cr>", desc = "[T]mux Rerun" },
-  },
-}
