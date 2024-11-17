2407ef7d32028ee9e36c209971065ebce0107d47
mvaldes <mvaldes.revilla@gmail.com>
2024-11-17 12:59:48 -0600

    fixing copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,41 +1,39 @@
 return {
-  "zbirenbaum/copilot.lua",
-  dependencies = {
-    "zbirenbaum/copilot-cmp",
-  },
-  cond = function()
-    return require("helper").check_work()
-  end,
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup {
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-y>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        copilot_node_command = "node", -- Node.js version must be > 16.x
-        server_opts_overrides = {},
-      }
-      -- require("copilot_cmp").setup {}
+  {
+    "zbirenbaum/copilot.lua",
+    cond = function()
+      return require("helper").check_work()
     end,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+          },
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            debounce = 75,
+            keymap = {
+              accept = "<C-y>",
+              accept_word = false,
+              accept_line = false,
+              next = "<M-]>",
+              prev = "<M-[>",
+              dismiss = "<C-]>",
+            },
+          },
+          copilot_node_command = "node", -- Node.js version must be > 16.x
+          server_opts_overrides = {},
+        }
+      }
+    end
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
