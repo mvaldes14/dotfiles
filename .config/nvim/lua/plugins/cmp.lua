6e6072609864584957368172faeef54ea71daa3b
mvaldes <mvaldes.revilla@gmail.com>
2024-12-23 16:49:54 -0600

    adding cmdline cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -9,7 +9,7 @@ return {
       nerd_font_variant = "mono",
     },
     sources = {
-      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      default = { "lsp", "path", "snippets", "buffer", "copilot", "cmdline" },
       providers = {
         copilot = {
           name = "copilot",
@@ -17,18 +17,6 @@ return {
           score_offset = 100,
           async = true,
         },
-        cmdline = function()
-          local type = vim.fn.getcmdtype()
-          -- Search forward and backward
-          if type == "/" or type == "?" then
-            return { "buffer" }
-          end
-          -- Commands
-          if type == ":" then
-            return { "cmdline" }
-          end
-          return {}
-        end,
       },
     },
     signature = { enabled = true },
