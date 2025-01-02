42f4c568649cea8a21dd6ae46bbdd9965acd01f1
mvaldes <mvaldes.revilla@gmail.com>
2025-01-02 09:21:30 -0600

    adding todoist from local

--- .config/nvim/lua/plugins/local-plugins.lua
+++ .config/nvim/lua/plugins/local-plugins.lua
@@ -9,4 +9,10 @@ return {
       return not require("helper").check_work()
     end,
   },
+  {
+    dir = "~/git/todoist.nvim",
+    cond = function()
+      return not require("helper").check_work()
+    end,
+  },
 }
