00cbd510dc5966bfc88eef91de6a61e051555e2e
a1471283 <a1471283@bestbuy.com>
2025-01-16 14:05:27 -0600

    picker over telescope

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,11 +3,17 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
+    quickfile = {
+      enabled = true,
+    },
+    scroll = {
+      enabled = true,
+    },
     notifier = {
       enabled = true,
       timeout = 3000,
     },
-    bifile = {
+    bigfile = {
       enabled = true,
       size = 1.0 * 1024 * 1024,
       notify = true,
