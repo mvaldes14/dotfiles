fc85eb8d9dac16fa8a01c360d40fee05bc495593
mvaldes <mvaldes.revilla@gmail.com>
2025-02-25 10:55:24 -0600

    flake for dotfiles and screenkey location

--- .config/nvim/lua/plugins/screenkey.lua
+++ .config/nvim/lua/plugins/screenkey.lua
@@ -1,6 +1,25 @@
 return {
-    "NStefan002/screenkey.nvim",
-    cmd = "Screenkey",
-    version = "*",
-    config = true,
+  "NStefan002/screenkey.nvim",
+  cmd = "Screenkey",
+  version = "*",
+  lazy = false,
+  config = function()
+    require("screenkey").setup {
+      win_opts = {
+        row = 0,
+        col = vim.o.columns,
+        relative = "editor",
+        anchor = "NE",
+        width = 40,
+        height = 3,
+        border = "single",
+        title = "Screenkey",
+        title_pos = "center",
+        style = "minimal",
+        focusable = false,
+        noautocmd = true,
+        zindex = 90,
+      },
+    }
+  end,
 }
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Base Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        inherit (pkgs) mkShell;
+      in {
+        devShells = {
+          default = mkShell {
+            buildInputs = with pkgs; [
+              lua52Packages.luacheck
+              stylua
+              lua-language-server
+            ];
+          };
+        };
+      };
+    };
+}
