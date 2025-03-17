336cf9d6f6b5f88f87619f520e1f562aff9e094c
mvaldes <mvaldes.revilla@gmail.com>
2025-03-17 09:07:42 -0500

    local plugin setting

--- .config/nvim/lua/plugins/local-plugins.lua
+++ .config/nvim/lua/plugins/local-plugins.lua
@@ -17,11 +17,13 @@ return {
     config = function()
       require("todoist").setup {
         token_api = "",
+        default_filter = "all",
         filters = {
           all = "all",
           today = "due:today",
           no_recurring = "!recurring",
           no_date = "no date",
+          pending = "@review",
         },
       }
     end,
