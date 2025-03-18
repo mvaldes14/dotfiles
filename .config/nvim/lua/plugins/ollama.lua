f36eafd174c9a6a16d6feb3fd4fcba4ec0a6493c
a1471283 <a1471283@bestbuy.com>
2025-03-18 09:52:53 -0500

    updating ollama prompts

--- .config/nvim/lua/plugins/ollama.lua
+++ .config/nvim/lua/plugins/ollama.lua
@@ -5,28 +5,29 @@ return {
     return not require("helper").check_work()
   end,
   opts = {
-    model = "mistral",
+    model = "gemma3",
     url = "http://192.168.1.218:11434",
     prompts = {
-      Fix_Grammar = {
-        prompt = "Fix the grammar and enhance the readability of the following text: \n$buf",
-        action = "replace",
-        model = "mistral",
-      },
-      Grammar_Selection = {
-        prompt = "Validate the grammar of the selected text: \n$sel",
+      FG = {
+        prompt =
+        "You are a tech blogger please help fix the grammar and enhance the readability of the following text: \n$buf",
         action = "display",
-        model = "mistral",
       },
-      Fix_Code = {
-        prompt = "Fix the code provided: \n$sel",
+      GS = {
+        prompt = "Validate that the grammar of the selected text is correct and makes sense: \n$sel",
         action = "display",
-        model = "mistral",
       },
-      Translate = {
-        prompt = "Translate the content to spanish: \n$buf",
+      FC = {
+        prompt = "You are an expert coder please help fix the following code: \n$sel",
         action = "display",
-        model = "mistral",
+      },
+      Trans = {
+        prompt = "Translate the content to spanish while keeping it consistent and readable: \n$buf",
+        action = "display",
+      },
+      EX = {
+        prompt = "You are an expert coder please help explain this code in a very concise way: \n$sel",
+        action = "display",
       },
     },
   },
