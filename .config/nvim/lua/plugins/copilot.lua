00449d52c773997c97a21d8ff669670cf365db72
a1471283 <a1471283@bestbuy.com>
2025-03-20 13:25:52 -0500

    fixing copilot chat

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -31,10 +31,12 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-    },
-    build = "make tiktoken",                          -- Only on MacOS or Linux
-  },
+    event = "InsertEnter",
+    opts = {
+      model = "gpt-4o-mini",
+      window = {
+        layout = "float",
+      },
+    }
+  }
 }
