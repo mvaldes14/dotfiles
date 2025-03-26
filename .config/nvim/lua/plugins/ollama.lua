b6d9b2b2c0ff3b63a1329533e652aa0dd089abb7
mvaldes <mvaldes.revilla@gmail.com>
2025-03-25 23:23:17 -0500

    ollama updates

--- .config/nvim/lua/plugins/ollama.lua
+++ .config/nvim/lua/plugins/ollama.lua
@@ -1,32 +1,23 @@
 return {
   "nomnivore/ollama.nvim",
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+  cmd = { "Ollama", "OllamaModel" },
   cond = function()
     return not require("helper").check_work()
   end,
   opts = {
-    model = "gemma3",
+    model = "gemma3:4b",
     url = "http://192.168.1.218:11434",
     prompts = {
-      FG = {
-        prompt =
-        "You are a tech blogger please help fix the grammar and enhance the readability of the following text: \n$buf",
+      Fix_Grammar = {
+        prompt = "You are a tech blogger please help fix the grammar and enhance the readability of the following text: \n$buf",
         action = "display",
       },
-      GS = {
+      Grammar_Check = {
         prompt = "Validate that the grammar of the selected text is correct and makes sense: \n$sel",
         action = "display",
       },
-      FC = {
-        prompt = "You are an expert coder please help fix the following code: \n$sel",
-        action = "display",
-      },
-      Trans = {
-        prompt = "Translate the content to spanish while keeping it consistent and readable: \n$buf",
-        action = "display",
-      },
-      EX = {
-        prompt = "You are an expert coder please help explain this code in a very concise way: \n$sel",
+      Translate = {
+        prompt = "Translate the content to Spanish while keeping it consistent and readable: \n$buf",
         action = "display",
       },
     },
