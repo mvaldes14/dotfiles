a4eb4e012baa16303db2bbde3ae502491f55390c
mvaldes <mvaldes.revilla@gmail.com>
2025-03-26 23:36:02 -0500

    mac on flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
 
       perSystem = {
         pkgs,
