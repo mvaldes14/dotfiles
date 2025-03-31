9b256b5a0ae23c62ad90c39b135063f54ddfabcc
a1471283 <a1471283@bestbuy.com>
2025-03-31 12:25:48 -0500

    copilot avante setup

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -4,17 +4,12 @@ return {
   version = false,
   opts = {
     provider = "copilot",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    },
   },
   build = "make",
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-  },
+  }
 }
