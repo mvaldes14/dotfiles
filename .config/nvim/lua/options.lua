64416b2d30b792b37c2c02c76dae5304570d15d1
mvaldes <mvaldes.revilla@gmail.com>
2025-04-13 21:45:10 -0500

    ghostty config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -24,6 +24,7 @@ local options = {
   termguicolors = true, -- set term gui colors (most terminals support this)
   timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true, -- enable persistent undo
+  undodir = "/tmp/nvim/undodir", -- set the directory for persistent undo (make sure this exists)
   updatetime = 300, -- faster completion (4000ms default)
   writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true, -- convert tabs to spaces
