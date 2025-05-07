df5c393f88eaa5f24f38ec862197cc05916c24e7
mvaldes <mvaldes.revilla@gmail.com>
2025-05-07 08:50:38 -0500

    setting up code companion

--- .config/nvim/lua/plugins/code-companion.lua
+++ .config/nvim/lua/plugins/code-companion.lua
@@ -1,4 +1,21 @@
 return {
   "olimorris/codecompanion.nvim",
   event = "InsertEnter",
+  opts = {
+    sources = {
+      per_filetype = {
+        codecompanion = { "codecompanion" },
+      },
+    },
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        },
+      },
+    },
+  },
 }
