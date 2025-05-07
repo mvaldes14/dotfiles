a8883009a434dfed45419445fb8158bdc90785d9
mvaldes <mvaldes.revilla@gmail.com>
2025-05-07 16:40:24 -0500

    markdown render

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,8 @@
 return {
   "echasnovski/mini.nvim",
+  dependencies = {
+    "MeanderingProgrammer/render-markdown.nvim",
+  },
   config = function()
     require("mini.ai").setup {}
     require("mini.surround").setup()
