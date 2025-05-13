deb84e59f82a4102c4c0852d09604f98cc9bf8b8
a1471283 <a1471283@bestbuy.com>
2025-05-13 11:22:09 -0500

    aerospace and noice

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -13,23 +13,13 @@ return {
         ["vim.lsp.util.stylize_markdown"] = false,
         ["cmp.entry.get_documentation"] = false,
       },
-      hover = {
-        enabled = true,
-      },
-      progress = {
-        enbaled = false,
-      },
     },
-    -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
+      bottom_search = false, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
   },
-  config = function(_, opts)
-    require("noice").setup(opts)
-  end,
 }
