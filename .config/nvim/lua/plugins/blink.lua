8008e49190ff4a25a637952d39b10f057267dc52
mvaldes14 <elxilote@gmail.com>
2025-12-16 12:06:02 -0600

    blink git completion for doing everything in cli

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,45 +1,51 @@
 return {
-    "saghen/blink.cmp",
-    dependencies = {
-        "rafamadriz/friendly-snippets",
-        "giuxtaposition/blink-cmp-copilot",
-        "mikavilpas/blink-ripgrep.nvim"
+  "saghen/blink.cmp",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "giuxtaposition/blink-cmp-copilot",
+    "mikavilpas/blink-ripgrep.nvim",
+    'Kaiser-Yang/blink-cmp-git',
+  },
+  version = "v0.*",
+  opts = {
+    keymap = { preset = "default" },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
     },
-    version = "v0.*",
-    opts = {
-        keymap = { preset = "default" },
-        appearance = {
-            use_nvim_cmp_as_default = true,
-            nerd_font_variant = "mono",
-        },
-        cmdline = {
-            enabled = false,
-        },
-        term = {
-            enabled = false,
-        },
-        sources = {
-            default = { "lazydev", "lsp", "path", "snippets", "buffer", "copilot", "ripgrep" },
-            providers = {
-                copilot = {
-                    name = "copilot",
-                    module = "blink-cmp-copilot",
-                    score_offset = 100,
-                    async = true,
-                },
-                lazydev = {
-                    name = "LazyDev",
-                    module = "lazydev.integrations.blink",
-                    -- make lazydev completions top priority (see `:h blink.cmp`)
-                    score_offset = 100,
-                },
-                ripgrep = {
-                    module = "blink-ripgrep",
-                    name = "Ripgrep",
-                    opts = {},
-                },
-            },
-        },
-        signature = { enabled = true },
+    cmdline = {
+      enabled = false,
     },
+    term = {
+      enabled = false,
+    },
+    sources = {
+      default = { "lazydev", "lsp", "path", "snippets", "buffer", "copilot", "ripgrep", "git" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
+        ripgrep = {
+          module = "blink-ripgrep",
+          name = "Ripgrep",
+          opts = {},
+        },
+        git = {
+          module = 'blink-cmp-git',
+          name = 'Git',
+          opts = {},
+        },
+      },
+    },
+    signature = { enabled = true },
+  },
 }
