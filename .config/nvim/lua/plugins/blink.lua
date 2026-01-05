1c74361c6722ce6090296059e5559c1346b8ab5f
mvaldes14 <elxilote@gmail.com>
2026-01-05 12:13:30 -0600

    blink updates

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,54 +1,58 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    "giuxtaposition/blink-cmp-copilot",
-    "mikavilpas/blink-ripgrep.nvim",
-    'Kaiser-Yang/blink-cmp-git',
-  },
-  version = "v0.*",
-  opts = {
-    keymap = { preset = "default" },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
+    "saghen/blink.cmp",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+        "giuxtaposition/blink-cmp-copilot",
+        "mikavilpas/blink-ripgrep.nvim",
+        'Kaiser-Yang/blink-cmp-git',
     },
-    cmdline = {
-      enabled = false,
+    version = "1.*",
+    opts = {
+        keymap = { preset = "default" },
+        appearance = {
+            nerd_font_variant = "mono",
+        },
+        signature = { window = { border = 'single' } },
+        completion = {
+            menu = {
+                border = 'single',
+            },
+            ghost_text = { enabled = true },
+        },
+        cmdline = {
+            enabled = false,
+        },
+        term = {
+            enabled = false,
+        },
+        sources = {
+            default = { "buffer", "lazydev", "lsp", "path", "snippets", "copilot", "ripgrep", "git" },
+            providers = {
+                copilot = {
+                    name = "copilot",
+                    module = "blink-cmp-copilot",
+                    score_offset = 100,
+                    async = true,
+                },
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100,
+                },
+                ripgrep = {
+                    module = "blink-ripgrep",
+                    name = "Ripgrep",
+                    opts = {},
+                },
+                git = {
+                    module = 'blink-cmp-git',
+                    name = 'Git',
+                    opts = {},
+                    enabled = function()
+                        return vim.tbl_contains({ 'git', 'markdown', 'gitcommit' }, vim.bo.filetype)
+                    end
+                },
+            },
+        },
     },
-    term = {
-      enabled = false,
-    },
-    sources = {
-      default = { "buffer", "lazydev", "lsp", "path", "snippets", "buffer", "copilot", "ripgrep", "git" },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          score_offset = 100,
-          async = true,
-        },
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          -- make lazydev completions top priority (see `:h blink.cmp`)
-          score_offset = 100,
-        },
-        ripgrep = {
-          module = "blink-ripgrep",
-          name = "Ripgrep",
-          opts = {},
-        },
-        git = {
-          module = 'blink-cmp-git',
-          name = 'Git',
-          opts = {},
-          enabled = function()
-            return vim.tbl_contains({ 'git', 'markdown', 'gitcommit' }, vim.bo.filetype)
-          end
-        },
-      },
-    },
-    signature = { enabled = true },
-  },
 }
